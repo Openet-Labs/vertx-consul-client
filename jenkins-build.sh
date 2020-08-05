f7d0d4129e952c7122712142efd5b3a05c7cd605
michalm <michalm@openet.com>
2020-08-05 10:56:46 +0200

    Add jenkins build file

---
+++ jenkins-build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -ex
+
+#mvn clean install
+
+VERSION=`grep -Pom2 "(?<=<version>)[^<]+" "pom.xml"|tail -1`
+
+if [[ "$VERSION" =~ "-SNAPSHOT" ]]
+then
+  USERNAME=$SNAPSHOT_USERNAME
+  PASSWORD=$SNAPSHOT_PASSWORD
+  URL=http://artifactory.openet-telecom.lan:8081/artifactory/ext-snapshot-local
+else
+  USERNAME=$RELEASE_USERNAME
+  PASSWORD=$RELEASE_PASSWORD
+  URL=http://artifactory.openet-telecom.lan:8081/artifactory/ext-release-local
+fi
+
+echo mvn -s settings.xml deploy:deploy-file -DrepositoryId=artifactory -Durl=$URL -DpomFile=pom.xml -Dfile=target/vertx-consul-client-${VERSION}.jar -Dartifactory.username=${USERNAME} -Dartifactory.password=${PASSWORD}
+
+
